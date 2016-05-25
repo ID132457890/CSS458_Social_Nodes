03a56e50569c37369f3ce787084af0ea7daf84d6
nightjars <nightjars@gmail.com>
2016-05-24 17:06:17 -0700

    turned aggregator into a class

--- Milestone2/AnalysisAggregator.py
+++ Milestone2/AnalysisAggregator.py
@@ -3,31 +3,34 @@ import DataExporter
 import ModelMangler
 import numpy as np
 
-results = []
 
-def collector(model, round_totals, data_map):
-    results.append(round_totals)
+class AnalysisAggregator(object):
+    def __init__(self):
+        self.results = []
 
-def simple_exec(**kwargs):
-    for x in range(10):
-        m = Model.Model(**kwargs)
-        m.run_simulation()
+    def collector(self, model, round_totals, data_map):
+        self.results.append(round_totals)
 
-    total_posts = 0
-    total_recv = 0
-    total_friends = 0
-    total_enemies = 0
-    total_knowledge = 0
+    def simple_exec(self, **kwargs):
+        for x in range(10):
+            m = Model.Model(**kwargs)
+            m.run_simulation()
 
-    result_array = np.zeros((10,5))
-    for x in range(len(results)):
-        test = results[x]
-        for round in test:
-            result_array[x, 0] += round['num_messages_sent']
-            result_array[x, 1] += round['num_messages_received']
-            result_array[x, 2] += round['num_total_friend']
-            result_array[x, 3] += round['num_total_enemies']
-            result_array[x, 4] += round['num_knowledge']
+        total_posts = 0
+        total_recv = 0
+        total_friends = 0
+        total_enemies = 0
+        total_knowledge = 0
+
+        result_array = np.zeros((10,5))
+        for x in range(len(self.results)):
+            test = self.results[x]
+            for round in test:
+                result_array[x, 0] += round['num_messages_sent']
+                result_array[x, 1] += round['num_messages_received']
+                result_array[x, 2] += round['num_total_friend']
+                result_array[x, 3] += round['num_total_enemies']
+                result_array[x, 4] += round['num_knowledge']
 
         print ("avg sent %r \t\t std dev %r" % (np.average(result_array[:,0]), np.std(result_array[:,0])))
         print("avg rcv %r \t\t std dev %r" % (np.average(result_array[:, 1]), np.std(result_array[:, 1])))
@@ -35,22 +38,22 @@ def simple_exec(**kwargs):
         print("avg enemies %r \t\t std dev %r" % (np.average(result_array[:, 3]), np.std(result_array[:, 3])))
         print("avg known %r \t\t std dev %r" % (np.average(result_array[:, 4]), np.std(result_array[:, 4])))
 
+a = AnalysisAggregator()
 print ("Averages with 50 agents, 10 topics:")
-simple_exec(num_agents = 50, topics = 10, data_collector = DataExporter.DataExporter,
-                        data_collector_results = collector, log_level = 10)
+a.simple_exec(num_agents = 50, topics = 10, data_collector = DataExporter.DataExporter,
+              time_to_run = 20, data_collector_results = a.collector, log_level = 10)
 
-results = []
-
+a = AnalysisAggregator()
 print ("Averages with 200 agents, 10 topics:")
-simple_exec(num_agents=50, topics=10, data_collector=DataExporter.DataExporter,
-            data_collector_results=collector, log_level=10)
+a.simple_exec(num_agents=200, topics=10, data_collector=DataExporter.DataExporter,
+              time_to_run=20, data_collector_results=a.collector, log_level=10)
 
-results = []
-
+a = AnalysisAggregator()
 print ("Averages with 50 agents, 50 topics:")
-simple_exec(num_agents=50, topics=50, data_collector=DataExporter.DataExporter,
-            data_collector_results=collector, log_level=10)
-results = []
+a.simple_exec(num_agents=50, topics=50, data_collector=DataExporter.DataExporter,
+              time_to_run=20, data_collector_results=a.collector, log_level=10)
+
+a = AnalysisAggregator()
 print ("Averages with 200 agents, 50 topics:")
-simple_exec(num_agents=200, topics=50, data_collector=DataExporter.DataExporter,
-            data_collector_results=collector, log_level=10)
+a.simple_exec(num_agents=200, topics=50, data_collector=DataExporter.DataExporter,
+              time_to_run=20, data_collector_results=a.collector, log_level=10)
